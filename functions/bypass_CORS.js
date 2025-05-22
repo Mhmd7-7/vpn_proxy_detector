5d66901f967390423f01c3c14f071c9cc8420be6
Mhmd7-7 <fdgrr28@gmail.com>
2025-05-22 18:15:33 +0300

    Update bypass_CORS.js

--- functions/bypass_CORS.js
+++ functions/bypass_CORS.js
@@ -1,15 +1,11 @@
 exports.handler = async (event) => {
   const url = event.queryStringParameters.url;
-  const apiKey = "l11151-636tc1-940138-06n954";
-
   if (!url) {
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'Missing "ip" parameter' }),
     };
   }
-
-
   try {
     const response = await fetch(url);
     const data = await response.json();
