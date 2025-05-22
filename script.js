2dcffefe42e8b48f531b0e2152b650bed110c43b
Mhmd7-7 <fdgrr28@gmail.com>
2025-05-22 16:00:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -152,10 +152,16 @@ fetch("https://pro.ip-api.com/json/37.243.74.117?fields=66842623&key=ipapiq9SFY1
 
     // i change  ipgeolocation.io to proxycheck.io fix the display error
 function checkproxycheck(ip) {
-    fetch(`/functions/proxycheck?ip=${ip}`)
-      .then(res => res.json())
-      console.log(res);
-    }
+  fetch(`/functions/proxycheck?ip=${ip}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data); // Log the JSON response
+    })
+    .catch(error => {
+      console.error("Error checking proxy:", error);
+    });
+}
+
 
     // API 5: ipdata.co 
     function checkIpdata(ip) {
