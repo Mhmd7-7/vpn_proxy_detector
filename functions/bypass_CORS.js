30e0f603d9ff891c57078a2e9733085518d390f2
Mhmd7-7 <fdgrr28@gmail.com>
2025-05-22 19:27:52 +0300

    Update bypass_CORS.js

--- functions/bypass_CORS.js
+++ functions/bypass_CORS.js
@@ -10,7 +10,6 @@ exports.handler = async (event) => {
 const response = await fetch(url, {
   method: 'GET',
   referrer: 'https://proxycheck.io',
-  referrerPolicy: 'strict-origin-when-cross-origin'
     headers: {
       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
       'accept-encoding': 'gzip, deflate, br, zstd',
